079560468c30909bace325152ffcb005a6c40572
h2techjun <h2techjun@gmail.com>
2026-04-15 14:10:32 +0900

    deploy: v0.1.2+3 — /owner/teachers 라우트 + 모바일 viewport

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1553731913" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "523025568" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
