1013c3f66b817c9740b1b3db8027eb0f0d0a4474
h2techjun <h2techjun@gmail.com>
2026-04-15 14:44:54 +0900

    deploy: v0.1.3+4 - 태블릿 가로 모드 지원

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1493747093" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "446892180" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
